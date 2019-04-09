80ac5b663821df6b723d383fa2d31b93ca5f65ce
karlosdraco <karlosdraco27@yahoo.com>
2019-04-10 00:45:03 +0800

    Added user post UI 80% done

--- controllers/home-controller.js
+++ controllers/home-controller.js
@@ -1,6 +1,13 @@
 app.controller('home-controller', ['$scope', 'RestService', function($scope, RestService){
      
-    
+    $scope.showDropdown = 0;
+
+    $scope.dropdown = function(){
+        $scope.showDropdown++;
+        if($scope.showDropdown > 1){
+            $scope.showDropdown = 0;
+        }
+    }
 
      
 }]);
