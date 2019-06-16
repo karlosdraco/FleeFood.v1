9105a52164504bb515bb41948f05471824928d2f
karlosdraco <karlosdraco27@yahoo.com>
2019-06-17 01:27:25 +0800

    Changed base color yellow,added notification controller,removed async on feed

--- controllers/notification-controller.js
+++ controllers/notification-controller.js
@@ -1,18 +1,25 @@
-app.controller("feed-controller", ['$scope', '$rootScope','RestService', '$timeout', function($scope, $rootScope,RestService, $timeout){
+app.controller("order-controller", ['$scope', '$rootScope','RestService','$routeParams',function($scope, $rootScope,RestService, $routeParams){
 
+    var loadTime = 5000, //Load the data every second
+    errorCount = 0, //Counter for the server errors
+    loadPromise; //Pointer to the promise created by the Angular $timout service
     $scope.id;
     $scope.reportCount;
     $scope.foodModal = 0;
     $scope.orderFunc = 0;
     
-    
+    var getData = function(){
         RestService.getFeed().then(function(response){
             $scope.feed = response.data;
             $scope.id = $rootScope.data.id;
             //$scope.viewFood($scope.feed);
+            errorCount = 0;
+            nextLoad();
         }).catch(function() {
             $scope.feed = 'Server error';
+            nextLoad(++errorCount * 2 * loadTime);
         });
+    }
 
     $scope.foodModalOn = function(){
         $scope.foodModal++;
@@ -35,6 +42,28 @@ app.controller("feed-controller", ['$scope', '$rootScope','RestService', '$timeo
         }
     }
     
+   
+        var cancelNextLoad = function(){
+            $timeout.cancel(loadPromise);
+        }
+
+        var nextLoad = function(mill){
+            mill = mill || loadTime;
+
+            //Always make sure the last timeout is cleared before starting a new one
+            cancelNextLoad();
+            loadPromise = $timeout(getData, mill);
+        }
+
+        //Start polling the data from the server
+        getData();
+
+        //Always clear the timeout when the view is destroyed, otherwise it will keep polling and leak memory
+        $scope.$on('$destroy', function() {
+        cancelNextLoad();
+        });
+
+        $scope.data = 'Loading...';
 
         $scope.reportButton = function(index){
             var feedIndex = $scope.feed.indexOf(index);
@@ -56,4 +85,7 @@ app.controller("feed-controller", ['$scope', '$rootScope','RestService', '$timeo
                 priceFree.food_price = $scope.free;
             }
         }
+}]);
+
+
 }]);
